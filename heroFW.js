7256256c3de97df12494467a3e91bb696177b477
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-09-21 14:50:47 +0200

    add style, js and sample

--- heroFW.js
+++ heroFW.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
     var HeroFW = Object.getPrototypeOf(app).HeroFW = new app.Component("heroFW");
-    //HeroFW.debug = true;
+    HeroFW.debug = true;
     HeroFW.createdAt      = "2.0.0";
     HeroFW.lastUpdate     = "2.0.0";
     HeroFW.version        = "1";
@@ -8,8 +8,29 @@ module.exports = function(app){
     // HeroFW.loadingMsg     = "This message will display in the console when component will be loaded.";
     // HeroFW.requires       = [];
 
-    // HeroFW.prototype.onCreate = function(){
-    // do thing after element's creation
-    // }
+    HeroFW.prototype.onCreate = function(){
+        var heroFW = this;
+        heroFW.content = {$el : heroFW.$el.find('.heroFW__content'),};
+        heroFW.setHeight();
+    }
+
+    HeroFW.prototype.onResize = function(){
+        var heroFW = this;
+        heroFW.setHeight();
+    }
+
+    HeroFW.prototype.setHeight = function() {
+        var heroFW = this;
+        heroFW.log('here')
+        var heightBox = 0;
+        if(heroFW.$el.data('height') && heroFW.$el.data('height') != ""){
+            heightBox = heroFW.$el.data('height');
+            if(heroFW.$el.data('height') == "viewport")
+                heightBox = utils.getViewportHeight();
+        }
+        if(heightBox != 0)
+            heroFW.$el.css('height',heightBox);
+        return heroFW;
+    };
     return HeroFW;
 }
