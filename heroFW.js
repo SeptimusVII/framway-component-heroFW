af86ddda70a3add6b6a3de700020a60491717182
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-08-14 19:31:27 +0200

    Fix: move font-size property to heroFW__wrapper div instead of the actual lvl 1 div, to avoid mess with font size related margins

--- heroFW.js
+++ heroFW.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var HeroFW = Object.getPrototypeOf(app).HeroFW = new app.Component("heroFW");
     // HeroFW.debug = true;
     HeroFW.createdAt      = "2.0.0";
-    HeroFW.lastUpdate     = "2.0.0";
-    HeroFW.version        = "1";
+    HeroFW.lastUpdate     = "2.4.0";
+    HeroFW.version        = "1.0.1";
     // HeroFW.factoryExclude = true;
     // HeroFW.loadingMsg     = "This message will display in the console when component will be loaded.";
     // HeroFW.requires       = [];
